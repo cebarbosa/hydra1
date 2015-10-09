2ec93e6733a0b1e11d51a4e597f849fd9bca9551
Carlos Barbosa <kadu.barbosa@gmail.com>
2015-10-09 15:28:58 -0300

    Improved contrast in figure and changed colors.

--- plot_ssp_gradients.py
+++ plot_ssp_gradients.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 import matplotlib.gridspec as gridspec
 from scipy.optimize import curve_fit
+from scipy.interpolate import interp1d
 import brewer2mpl
 
 from config import *
@@ -102,7 +103,7 @@ if __name__ == "__main__":
     ##########################################################################
     # Set figure parameters
     gs = gridspec.GridSpec(4,5)
-    gs.update(left=0.08, right=0.98, bottom = 0.06, top=0.98, hspace = 0.12,
+    gs.update(left=0.08, right=0.98, bottom = 0.06, top=0.98, hspace = 0.14,
                wspace=0.085)
     fig = plt.figure(1, figsize = (12, 9))
     xcb = 0.21
@@ -113,7 +114,7 @@ if __name__ == "__main__":
     lgray = "0.85"
     dgray = "0.7"
     norm = Normalize(color.min(),color.max())
-    ylims = [[9.6,10.3], [-2.4,1.2], [-0.5,0.75], [-3.5, 1.5]]
+    ylims = [[9.7,10.2], [-2.4,1.2], [-0.5,0.75], [-3.5, 1.5]]
     tex, tex2, grad_tab1, grad_tab2 = [], [], [], []
     # Setting the colormap properties for the scatter plots
     cmap = brewer2mpl.get_map('Blues', 'sequential', 9).mpl_colormap
@@ -148,6 +149,30 @@ if __name__ == "__main__":
         #             z[k] = np.nan
         #             zerr[k] = np.nan
         ######################################################################
+        # Read data for sky p/m 1%
+        ys = ['age', "metal", "alpha", "iron"]
+        xs = ["r", 'mu']
+        f1 = os.path.join(tables_dir, "pm1pc_{0}_{1}.txt".format(
+                                     xs[0], ys[i]))
+        f2 = os.path.join(tables_dir, "pm1pc_{0}_{1}.txt".format(
+                                     xs[1], ys[i]))
+        f3 = os.path.join(tables_dir, "pm6pc_{0}_{1}.txt".format(
+                                     xs[0], ys[i]))
+        f4 = os.path.join(tables_dir, "pm6pc_{0}_{1}.txt".format(
+                                     xs[1], ys[i]))
+        sim1 = np.loadtxt(f1).T
+        sim2 = np.loadtxt(f2).T
+        sim3 = np.loadtxt(f3).T
+        sim4 = np.loadtxt(f4).T
+        rms1 = interp1d(sim1[0], sim1[1], kind="linear", bounds_error=0,
+                        fill_value=0.)
+        rms2 = interp1d(sim2[0], sim2[1], kind="linear", bounds_error=0,
+                        fill_value=0.)
+        rms3 = interp1d(sim3[0], sim3[1], kind="linear", bounds_error=0,
+                        fill_value=0.)
+        rms4 = interp1d(sim4[0], sim4[1], kind="linear", bounds_error=0,
+                        fill_value=0.)
+        ######################################################################
         # Plot data in radial distance
         ######################################################################
         ax = plt.subplot(gs[i,0:2], xscale="linear")
@@ -162,19 +187,19 @@ if __name__ == "__main__":
         ii2 =  np.where(sn < 20.)[0]
         ax.errorbar(r, y, yerr = [y1, y2], fmt=None,
                     color=lgray, ecolor=lgray, capsize=0., zorder=1)
-        ax.scatter(r, y, c=sn, s=45, cmap=cmap, zorder=2,
-                   lw=0.1, norm=norm)
+        ax.scatter(r, y, c=sn, s=50, cmap=cmap, zorder=2,
+                   lw=0.5, norm=norm)
         ######################################################################
         # Plot data for Loubser 2012
         ax.errorbar(r_l12, l12[i], yerr = [l12_errs1[i], l12_errs2[i]],
                     color="r", ecolor=lgray,
                         fmt="s", mec="k", capsize=0,
-                        alpha=1, ms=7, mew=0.2)
+                        alpha=1, ms=7.5, mew=0.5)
         # ax.yaxis.set_major_locator(plt.MaxNLocator(5))
         ax.set_xlim(-1.5, 0.8)
         ######################################################################
         # Plot data for Coccato+ 2011
-        ax.plot(rlodo, data_lodo[i], "^y", mec="k", ms=7, mew=0.1)
+        ax.plot(rlodo, data_lodo[i], "^", mec="k", c="orange", ms=8, mew=0.5)
         #####################################################################
         # Draw arrows to indicate central limits
         ax.annotate("", xy=(-1., loubser[i]), xycoords='data',
@@ -182,32 +207,24 @@ if __name__ == "__main__":
         arrowprops=dict(arrowstyle="<-", connectionstyle="arc3", ec="r",
                         lw=1.5))
         #####################################################################
-        # Read data for sky p/m 1%
-        # rsky, sky = np.loadtxt(os.path.join(tables_dir, "rms_1pc_re.txt"),
-        #                        usecols=(0,i+1)).T
-        # rms = interp1d(rsky, sky, kind="linear", bounds_error=0, fill_value=0.)
-        # musky, sky2 = np.loadtxt(os.path.join(tables_dir, "rms_1pc_sb.txt"),
-        #                        usecols=(0,i+1)).T
-        # rms2 = interp1d(musky, sky2, kind="linear", bounds_error=0,
-        #                 fill_value=0.)
-        #####################################################################
         # Second plot: data as function of surface brightness
         #####################################################################
         ax2.errorbar(mu, y, yerr = [y1, y2], fmt=None,
                     color=lgray, ecolor=lgray, capsize=0., zorder=1)
-        ax2.scatter(mu, y, c=sn, s=45, cmap=cmap, zorder=2,
-                   lw=0.1, norm=norm)
+        ax2.scatter(mu, y, c=sn, s=50, cmap=cmap, zorder=2,
+                   lw=0.5, norm=norm)
         ax2.set_xlim(19,25)
         ######################################################################
         # Plot data for Loubser 2012
         ax2.errorbar(l12_sb, l12[i], yerr = [l12_errs1[i], l12_errs2[i]],
                     color="r", ecolor=lgray,
                         fmt="s", mec="k", capsize=0,
-                        alpha=1, ms=7, mew=0.2)
+                        alpha=1, ms=7.5, mew=0.5)
         ######################################################################
         ######################################################################
         # Plot data for Coccato+ 2011
-        ax2.plot(mulodo, data_lodo[i], "^y", mec="k", ms=7, mew=0.1)
+        ax2.plot(mulodo, data_lodo[i], "^", mec="k", c="orange", ms=8,
+                 mew=0.5)
         #####################################################################
         if i == 3:
             plt.xlabel(r"$\mu_V$ (mag arcsec$^{-2}$)")
@@ -246,10 +263,19 @@ if __name__ == "__main__":
                     round(popt1[0],2), round(pcov1[0],2)),
                      xy=(0.06,0.35), xycoords="axes fraction", color="k",
                      fontsize=12)
+        # ax.plot(x, yy + rms1(x), "-y")
+        # ax.plot(x, yy - rms1(x), "-y")
+        # ax.plot(x, yy + rms3(x), "-y")
+        # ax.plot(x, yy - rms3(x), "-y")
+        ax.fill_between(x, yy - rms1(x), yy + rms1(x), edgecolor="none",
+                        color="0.5", linewidth=0, alpha=0.3)
         ######################################################################
         # Outer halo
         mask = ~np.isnan(yc2)
-        popt2, pcov2 = curve_fit(f, rc2[mask], yc2[mask])
+        if i == 0:
+            popt2, pcov2 = curve_fit(f, rc2[mask], yc2[mask], sigma=yc2err)
+        else:
+            popt2, pcov2 = curve_fit(f, rc2[mask], yc2[mask])
         pcov2 = np.sqrt(np.diagonal(pcov2) + 0.01**2)
         label = r"{1:.2f}$\pm${2:.2f}".format(
                     pars2[i], round(popt2[1],2), round(pcov2[1],2),
@@ -259,6 +285,12 @@ if __name__ == "__main__":
         lll, = ax.plot(x, yy, "-k", lw=1.5, alpha=0.9, zorder=1000,
                        label=label)
         lll.set_dashes([10, 3])
+        # ax.plot(x, yy + rms1(x), "-y")
+        # ax.plot(x, yy - rms1(x), "-y")
+        # ax.plot(x, yy + rms3(x), "-y")
+        # ax.plot(x, yy - rms3(x), "-y")
+        ax.fill_between(x, yy - rms1(x), yy + rms1(x), edgecolor="none",
+                        color="0.5", linewidth=0, alpha=0.3)
         #####################################################################
         ax.legend(loc=3, fontsize=12, frameon=0, handlelength=2.5)
         #######################################################################
@@ -282,9 +314,18 @@ if __name__ == "__main__":
                     round(popt1[0],2), round(pcov1[0],2)),
                      xy=(0.06,0.35), xycoords="axes fraction", color="k",
                      fontsize=12)
+        # ax2.plot(x, yy + rms2(x), "-y")
+        # ax2.plot(x, yy - rms2(x), "-y")
+        # ax2.plot(x, yy + rms4(x), "-y")
+        # ax2.plot(x, yy - rms4(x), "-y")
+        ax2.fill_between(x, yy - rms2(x), yy + rms2(x), edgecolor="none",
+                        color="0.5", linewidth=0, alpha=0.3)
         ######################################################################
         # Outer halo
-        popt4, pcov4 = curve_fit(f, sbc2[mask], yc2[mask])
+        if i == 0:
+            popt4, pcov4 = curve_fit(f, sbc2[mask], yc2[mask], sigma=yc2err[mask])
+        else:
+            popt4, pcov4 = curve_fit(f, sbc2[mask], yc2[mask])
         pcov4 = np.sqrt(np.diagonal(pcov4) + 0.01**2)
         label = r"{1:.2f}$\pm${2:.2f}".format(
                     pars2[i], round(popt4[1],2), round(pcov4[1],2),
@@ -294,6 +335,12 @@ if __name__ == "__main__":
         lll, = ax2.plot(x, yy, "-k", lw=1.5, alpha=0.9, zorder=1000,
                        label=label)
         lll.set_dashes([10, 3])
+        # ax2.plot(x, yy + rms2(x), "-y")
+        # ax2.plot(x, yy - rms2(x), "-y")
+        # ax2.plot(x, yy + rms4(x), "-y")
+        # ax2.plot(x, yy - rms4(x), "-y")
+        ax2.fill_between(x, yy - rms2(x), yy + rms2(x), edgecolor="none",
+                        color="0.5", linewidth=0, alpha=0.3)
         #####################################################################
         ax2.legend(loc=3, fontsize=12, frameon=0, handlelength=2.5)
         #######################################################################
